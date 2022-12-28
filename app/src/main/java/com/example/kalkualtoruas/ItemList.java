803fb82996e43e6d4a068269a2cfcecc1af41c2e
Muhammad Eky Ramadhan <ekyramadhan054@gmail.com>
2022-12-28 21:42:36 +0700

    Commit

--- app/src/main/java/com/example/kalkualtoruas/ItemList.java
+++ app/src/main/java/com/example/kalkualtoruas/ItemList.java
@@ -1,31 +1,31 @@
 package com.example.kalkualtoruas;
 
 public class ItemList {
-    private final String mAngka1;
-    private final String mLineOperasi;
-    private final String mAngka2;
-    private final String mLineHasil;
+    private final String Angka1;
+    private final String LineOperasi;
+    private final String Angka2;
+    private final String LineHasil;
 
     public ItemList(String angka1, String lineOperasi, String angka2, String lineHasil) {
-        mAngka1 = angka1;
-        mLineOperasi = lineOperasi;
-        mAngka2 = angka2;
-        mLineHasil = lineHasil;
+        Angka1 = angka1;
+        LineOperasi = lineOperasi;
+        Angka2 = angka2;
+        LineHasil = lineHasil;
     }
 
     public String getLine1() {
-        return mAngka1;
+        return Angka1;
     }
 
     public String getLineOperasi() {
-        return mLineOperasi;
+        return LineOperasi;
     }
 
     public String getLine2() {
-        return mAngka2;
+        return Angka2;
     }
 
     public String getLineHasil() {
-        return mLineHasil;
+        return LineHasil;
     }
 }
